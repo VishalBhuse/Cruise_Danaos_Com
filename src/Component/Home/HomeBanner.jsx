3d764a8c532cb0bec0c214a3de9c3c604d233c80
VishalBhuse <vbhuse1997@gmail.com>
2023-05-01 19:27:45 +0530

    background video add

--- src/Component/Home/HomeBanner.jsx
+++ src/Component/Home/HomeBanner.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Box, Text, Button, HStack, VStack } from "@chakra-ui/react";
 
-import { HiOutlineArrowRight } from "react-icons/hi";
+// import { HiOutlineArrowRight } from "react-icons/hi";
 import { Link } from "react-router-dom";
 const HomeBanner = () => {
   return (
@@ -9,39 +9,95 @@ const HomeBanner = () => {
       <Box w="100%" mx="auto">
         <Box
           w="100%"
-          bg="url('./img/vishal/homeban.png')"
-          backgroundPosition="center"
-          backgroundRepeat="no-repeat"
-          backgroundSize="cover"
           height={{ base: "200px", md: "400px", lg: "600px" }}
+          position="relative"
+          overflow="hidden"
         >
-          <Box w="90%" mx="auto">
-            <Box w={["60%", "60%", "50%"]} mt="0.5rem">
+          <video
+            src="./img/vishal/boat-video.mp4"
+            autoPlay
+            muted
+            loop
+            style={{
+              minWidth: "100%",
+              zIndex: 1,
+              position: "absolute",
+              top: "30%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+            }}
+          ></video>
+          <Box
+            w="90%"
+            mx="auto"
+            position={"absolute"}
+            top="30%"
+            left=" 50%"
+            transform="translate(-50%, -50%)"
+            zIndex={2}
+          >
+            <Box mt="0.5rem">
               <VStack
-                justifyContent={"flex-start"}
+                textAlign={"center"}
+                alignItems={"center"}
+                justifyContent={"center"}
                 spacing={{ base: "20px", md: "20px", lg: "50px" }}
               >
                 <Box>
                   <Text
-                    mt={{ base: "10px", md: "15px" }}
+                    w={"full"}
                     fontSize={{
-                      base: "10px",
-                      sm: "15px",
+                      base: "15px",
+                      sm: "18px",
                       md: "25px",
-                      lg: "35px",
-                      xl: "35px",
+                      lg: "20px",
+                      xl: "20px",
                     }}
-                    color={"#081839"}
-                    fontWeight={"700"}
+                    fontWeight={800}
+                    color={"#f39c12"}
+                    fontFamily={"Josefin Sans"}
+                    textTransform={"capitalize"}
                   >
-                    Living the cruise life. <br /> A bad day on a cruise is
-                    better than a good day anywhere else.
+                    WELCOME TO DANAOS
+                  </Text>
+                  <Text
+                    w={"full"}
+                    color={"#081839"}
+                    fontSize={{
+                      base: "30px",
+                      sm: "30px",
+                      md: "40px",
+                      lg: "80px",
+                      xl: "80px",
+                    }}
+                    zIndex={1}
+                    fontWeight={900}
+                    fontFamily={"Playfair Display"}
+                    textTransform={"capitalize"}
+                  >
+                    Nauti Amigo
+                  </Text>
+                  <Text
+                    w={"full"}
+                    fontSize={{
+                      base: "15px",
+                      sm: "18px",
+                      md: "25px",
+                      lg: "30px",
+                      xl: "30px",
+                    }}
+                    fontWeight={800}
+                    color={"white"}
+                    fontFamily={"Josefin Sans"}
+                    textTransform={"capitalize"}
+                  >
+                    India's Most Luxurious Dinner Cruise In GOA.
                   </Text>
                   <Link to="/allpackages">
                     <Button
                       mt="3"
                       background={
-                        "linear-gradient(90deg, #F9D33B 0%, #F8ECC1 100%)"
+                        "linear-gradient(90deg, #f39c12 0%, #F2CD5C 100%)"
                       }
                       _hover={{
                         background:
@@ -57,104 +113,6 @@ const HomeBanner = () => {
                   </Link>
                 </Box>
               </VStack>
-            </Box>
-            <Box mt="3" display={["none", "none", "none", "block"]}>
-              <HStack>
-                <Box
-                  width={"207px"}
-                  height={["308px"]}
-                  background={"rgba(8, 24, 57, 0.59)"}
-                  borderRadius={"10px"}
-                  display="flex"
-                  justifyContent={"space-between"}
-                  flexDirection="column"
-                  px="2"
-                  py="2"
-                  color={"#FFFFFF"}
-                >
-                  <Text fontWeight={"700"} fontSize={"32px"} textAlign="center">
-                    Tour
-                  </Text>
-                  <Text
-                    fontWeight={"500"}
-                    fontSize={"12px"}
-                    textAlign="justify"
-                  >
-                    Lovers of all things nautical and anyone who’d like to add
-                    some luxe to their Instagram – won’t want to miss out on the
-                    Nauti Amigo, a classical Danaos ship remodeled as an
-                    indulgent superyacht. Enjoy a to-the-point small-group
-                    guided tour with ample photo opportunities.
-                  </Text>
-                  <Box w="100%" mx="auto" textAlign={"center"}>
-                    <Link to="/allpackages">
-                      <Button
-                        px={{ base: "2", md: "4" }}
-                        py={{ base: "1", md: "2" }}
-                        fontWeight="700"
-                        fontSize={{ base: "10px", md: "14px" }}
-                        _hover={{
-                          background:
-                            "linear-gradient(90deg, #F9D33B 0%, #F8ECC1 100%)",
-                        }}
-                        background="linear-gradient(90deg, #F9D33B 0%, #F8ECC1 100%)"
-                        textTransform={"capitalize"}
-                        color="#081839"
-                      >
-                        view more &nbsp;
-                        <HiOutlineArrowRight />
-                      </Button>
-                    </Link>
-                  </Box>
-                </Box>
-                <Box
-                  width={"207px"}
-                  height={["308px"]}
-                  background={"rgba(8, 24, 57, 0.59)"}
-                  borderRadius={"10px"}
-                  display="flex"
-                  justifyContent={"space-between"}
-                  flexDirection="column"
-                  px="2"
-                  py="2"
-                  color={"#FFFFFF"}
-                >
-                  <Text fontWeight={"700"} fontSize={"32px"} textAlign="center">
-                    Locations
-                  </Text>
-                  <Text
-                    fontWeight={"500"}
-                    fontSize={"12px"}
-                    textAlign="justify"
-                  >
-                    Celebrate a special occasion such as an anniversary,
-                    birthday or proposal on the Nauti Amigo. Make your memory
-                    unique on board India's  only Luxury Danaos. Find special
-                    deals and offers exclusively on India's  only Luxury Danaos;
-                    The Nauti Amigo
-                  </Text>
-                  <Box w="100%" mx="auto" textAlign={"center"}>
-                    <Link to="/allpackages">
-                      <Button
-                        px={{ base: "2", md: "4" }}
-                        py={{ base: "1", md: "2" }}
-                        fontWeight="700"
-                        fontSize={{ base: "10px", md: "14px" }}
-                        _hover={{
-                          background:
-                            "linear-gradient(90deg, #F9D33B 0%, #F8ECC1 100%)",
-                        }}
-                        background="linear-gradient(90deg, #F9D33B 0%, #F8ECC1 100%)"
-                        textTransform={"capitalize"}
-                        color="#081839"
-                      >
-                        view more &nbsp;
-                        <HiOutlineArrowRight />
-                      </Button>
-                    </Link>
-                  </Box>
-                </Box>
-              </HStack>
             </Box>
           </Box>
         </Box>
